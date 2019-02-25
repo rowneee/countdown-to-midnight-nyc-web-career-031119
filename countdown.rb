4f36a6d0ae43214081202641c58c90bb7373d0ae
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 17:24:47 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(num)
   while num > 0
    puts "#{num} SECOND(S)!"
-   counter -= 1
+   num -= 1
   end
 end
 
@@ -11,5 +11,5 @@ def countdown_with_sleep(num)
   while num > 0 
   puts "#{num} SECOND(S)!"
   sleep(1)
-  
+  num -= 1 
 end
